88ac7a924ba76307ecd15ea8a87937063dc86f6e
connor <connor@cs.brown.edu>
2013-12-09 21:38:59 -0500

    Refactor map code

--- Platform/app/scripts/directives/geolocTimeMap.js
+++ Platform/app/scripts/directives/geolocTimeMap.js
@@ -33,7 +33,9 @@ angular.module('prototypeApp')
               .projection(projection);
 
           // Append all the map core features like roads, lakes, etc.
-          svg.append('g').selectAll('g')
+          var map = svg.append('g');
+
+          map.selectAll('g')
               .data(tiler
                 .scale(projection.scale() * 2 * Math.PI)
                 .translate(projection([0, 0])))
@@ -57,7 +59,7 @@ angular.module('prototypeApp')
               });
 
           // Append the points from data
-          var locCircles = svg.append('g').selectAll('circle')
+          var locCircles = map.append('g').selectAll('circle')
             .data(data)
             .enter()
               .append('circle')
@@ -70,82 +72,7 @@ angular.module('prototypeApp')
                 .attr('r', 3)
                 .attr('fill', 'rgba(50,200,50,1');
 
-          // Timeline and brushing support
-          // Inspired by http://bl.ocks.org/mbostock/4349545
-          var dates = data.map(function(d) { return d.date; }),
-              maxDate = new Date(Math.max.apply(null,dates)),
-              minDate = new Date(Math.min.apply(null,dates));
-
-          var margin = {top: 0, right: 17, bottom: 75, left: 17},
-              brushWidth = 900 - margin.left - margin.right,
-              brushHeight = 100 - margin.top - margin.bottom;
-
-          var dateX = d3.time.scale()
-              .domain([minDate, maxDate])
-              .range([0, brushWidth]);
-
-          var brush = d3.svg.brush()
-            .x(dateX)
-            .extent([.3, .5])
-            .on("brushstart", brushstart)
-            .on("brush", brushmove)
-            .on("brushend", brushend);
-
-          var arc = d3.svg.arc()
-            .outerRadius(brushHeight / 4)
-            .startAngle(0)
-            .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
-
-          var svgBrush = d3.select("body").append("svg")
-            .attr("width", brushWidth + margin.left + margin.right)
-            .attr("height", brushHeight + margin.top + margin.bottom)
-          .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-          svgBrush.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + brushHeight + ")")
-            .call(d3.svg.axis().scale(dateX).orient("bottom"));
-
-          var circle = svgBrush.append("g").selectAll("circle")
-            .data(data)
-          .enter().append("circle")
-            .attr("transform", function(d) { return "translate(" + dateX(d.date) + "," + brushHeight/2 + ")"; })
-            .attr("r", 3.5);
-
-          var brushg = svgBrush.append("g")
-            .attr("class", "brush")
-            .call(brush);
-
-          brushg.selectAll(".resize").append("path")
-            .attr("transform", "translate(0," +  brushHeight / 2 + ")")
-            .attr("d", arc);
-
-          brushg.selectAll("rect")
-            .attr("height", brushHeight);
-
-          brushstart();
-          brushmove();
-
-          function brushstart() {
-            svgBrush.classed("selecting", true);
-          }
-
-          function brushmove() {
-            var s = brush.extent();
-            circle.classed('selected', function(d) { return s[0] <= d.date && d.date <= s[1]; });
-            locCircles.classed('selectedGeoLoc',  function(d) {
-              return s[0] <= d.date && d.date <= s[1];
-            });
-            locCircles.classed('notSelectedGeoLoc', function(d) {
-              return !(s[0] <= d.date && d.date <= s[1]);
-            });
-          }
-
-          function brushend() {
-            svgBrush.classed("selecting", !d3.event.target.empty());
-          }
-        });
+        }); // end data request
       }
     };
   });
